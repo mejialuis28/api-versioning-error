dacefe9c2818ee893487e059334f96a58004abcf
luismejia-iq <LuisM@iqmetrix.com>
2022-09-06 15:44:41 -0500

    api version issue

---
+++ ApiVersioning/Controllers/Data2Controller.cs
@@ -0,0 +1,20 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ApiVersioning.Controllers
+{
+	[ApiVersion("2.0")]
+	[ApiController]
+	[Route("/v{version:apiVersion}/Data")]
+	public class Data2Controller : ControllerBase
+	{
+
+		[HttpGet()]
+		public async Task<IActionResult> Get()
+		{
+			// Some async call
+			return Ok("OK from V2");
+		}
+	}
+}
---
+++ ApiVersioning/Controllers/DataController.cs
@@ -0,0 +1,20 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ApiVersioning.Controllers
+{
+	[ApiVersion("1.0")]
+	[ApiController]
+	[Route("[controller]")]
+	[Route("/v{version:apiVersion}/[controller]")]
+	public class DataController : ControllerBase
+	{
+		[HttpGet()]
+		public async Task<IActionResult> Get()
+		{
+			// Some async call
+			return Ok("OK from V1");
+		}
+	}
+}
